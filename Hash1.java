6ce6c0479b856b16f5c40235550ccedb27c8f156
Sreenivas <srini0014@gmail.com>
2016-12-27 16:58:06 +0530

    hashset & linked list
    creation of hashset & linked list

---
+++ Hash1.java
@@ -0,0 +1,23 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+
+public class Hash1 {
+
+	public static void main(String[] args) {
+		HashSet<String> hs= new HashSet<String>();
+			hs.add("s");
+			hs.add("a");
+			hs.add("f");
+	
+		LinkedList<String> l1 = new LinkedList<String>();
+		l1.add("sddsad");
+		l1.add("xcc");
+		l1.add("dss");
+			
+			System.out.println(hs);
+			System.out.println(l1);
+			
+			}
+
+}
+
